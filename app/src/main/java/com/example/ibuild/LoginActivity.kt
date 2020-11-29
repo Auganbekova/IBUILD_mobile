6afa20779aa7c4d7d4788c29314a0fb57502a8b6
Auganbekova <aikowa2212@gmail.com>
2020-11-29 23:44:48 +0600

    добавила проверку email

--- app/src/main/java/com/example/ibuild/LoginActivity.kt
+++ app/src/main/java/com/example/ibuild/LoginActivity.kt
@@ -1,11 +1,21 @@
 package com.example.ibuild
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        setUpViews()
+    }
+    private fun setUpViews(){
+        dh_account.setOnClickListener {
+            val intent = Intent(this, RegistrationActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/ibuild/RegistrationActivity.kt
+++ app/src/main/java/com/example/ibuild/RegistrationActivity.kt
@@ -2,10 +2,27 @@ package com.example.ibuild
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_registration.*
 
 class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        setUpViews()
+    }
+    private fun setUpViews(){
+        txt_email.setOnFocusChangeListener { v, hasFocus ->
+            if (!hasFocus){
+                if (txt_email.text.toString().length < 6){
+                    Log.d("checklength", "no")
+                }
+                else{
+                    Log.d("checklength","yes")
+                }
+            }
+
+        }
     }
 }
