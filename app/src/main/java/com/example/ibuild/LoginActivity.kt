b794042ceedae8d7bf1ffd5170a2e411c15e2644
Auganbekova <aikowa2212@gmail.com>
2020-12-10 11:05:10 +0600

    добавила меню

--- app/src/main/java/com/example/ibuild/LoginActivity.kt
+++ app/src/main/java/com/example/ibuild/LoginActivity.kt
@@ -3,19 +3,41 @@ package com.example.ibuild
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    private val auth by lazy{
+        FirebaseAuth.getInstance()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         setUpViews()
     }
     private fun setUpViews(){
+        login.setOnClickListener{
+            signIn(txt_email.text.toString(), txt_password.text.toString())
+        }
         dh_account.setOnClickListener {
             val intent = Intent(this, RegistrationActivity::class.java)
             startActivity(intent)
+
         }
 
+
+    }
+    private fun signIn(email:String, password:String){
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener{ task ->
+            if (task.isSuccessful){
+                Toast.makeText(this, "Success", Toast.LENGTH_LONG).show()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                return@addOnCompleteListener
+
+            }
+            Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+        }
     }
 }
