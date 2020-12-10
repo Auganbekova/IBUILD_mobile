bcc7419ebbff1fec10275d29a11fcef445f4b706
Auganbekova <aikowa2212@gmail.com>
2020-12-10 13:19:22 +0600

    Исправила вид меню

--- app/src/main/java/com/example/ibuild/RegistrationActivity.kt
+++ app/src/main/java/com/example/ibuild/RegistrationActivity.kt
@@ -5,13 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.example.ibuild.data_classes.User
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_registration.*
 
 class RegistrationActivity : AppCompatActivity() {
-    private val auth by lazy{
-        FirebaseAuth.getInstance()
-    }
+    private val auth by lazy { FirebaseAuth.getInstance() }
+    private val database by lazy { FirebaseFirestore.getInstance() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +21,32 @@ class RegistrationActivity : AppCompatActivity() {
         setUpViews()
     }
 
-    private fun signUp(email:String, password:String){
-        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener{ task ->
-            if (task.isSuccessful){
+    private fun setUpViews() {
+
+        txt_email.setOnFocusChangeListener { v, hasFocus ->
+            if (!hasFocus) {
+                if (txt_email.text.toString().length < 6) {
+                    Log.d("checklength", "no")
+                } else {
+                    Log.d("checklength", "yes")
+                }
+            }
+        }
+
+        register.setOnClickListener {
+            val name = txt_name.text.toString()
+            val surname = txt_surname.text.toString()
+            val email = txt_email.text.toString()
+            val password = txt_password.text.toString()
+            signUp(email, name, surname, password)
+        }
+    }
+
+    private fun signUp(email: String, name: String, surname: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
                 Toast.makeText(this, "Success", Toast.LENGTH_LONG).show()
+                loadUserData(task.result?.user?.uid!!, User(task.result?.user?.uid!!, email, name, surname))
                 val intent = Intent(this, LoginActivity::class.java)
                 startActivity(intent)
                 return@addOnCompleteListener
@@ -33,26 +56,14 @@ class RegistrationActivity : AppCompatActivity() {
         }
     }
 
-    private fun setUpViews() {
-
-        txt_email.setOnFocusChangeListener { v, hasFocus ->
-            if (!hasFocus){
-                if (txt_email.text.toString().length < 6){
-                    Log.d("checklength", "no")
-                }
-                else{
-                    Log.d("checklength","yes")
-                }
+    private fun loadUserData(
+        id: String,
+        user: User
+    ) {
+        database.collection("users")
+            .document(id)
+            .set(user).addOnSuccessListener {
+                Toast.makeText(this, "Student data loaded", Toast.LENGTH_LONG).show()
             }
-
-        }
-
-        register.setOnClickListener {
-            val email = txt_email.text.toString()
-            val password = txt_password.text.toString()
-            signUp(email, password)
-
-        }
-
     }
 }
