6fe273e0458172ada34aa8757fc25aa44dc55efd
Auganbekova <aikowa2212@gmail.com>
2020-12-10 17:29:57 +0600

    Сделала переходы между окнами

--- app/src/main/java/com/example/ibuild/ChatListActivity.kt
+++ app/src/main/java/com/example/ibuild/ChatListActivity.kt
@@ -3,24 +3,23 @@ package com.example.ibuild
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_chat_list.*
 
-class MainActivity : AppCompatActivity() {
+class ChatListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_chat_list)
 
         setupViews()
     }
 
     private fun setupViews(){
-        btn_work_chat.setOnClickListener {
-            val intent = Intent(this, ChatListActivity::class.java)
+        btn_chat_profile.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
             startActivity(intent)
         }
-
-        btn_work_profile.setOnClickListener {
-            val intent = Intent(this, ProfileActivity::class.java)
+        btn_chat_work.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/ibuild/ProfileActivity.kt
+++ app/src/main/java/com/example/ibuild/ProfileActivity.kt
@@ -1,11 +1,27 @@
 package com.example.ibuild
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        setupViews()
     }
+
+    private fun setupViews(){
+        btn_profile_work.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        btn_profile_chat.setOnClickListener {
+            val intent = Intent(this, ChatListActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
 }
